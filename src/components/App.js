b84c059509419092d0843e554d9e80b401b2cd06
mannmanjeet988 <mannmanjeet988@gmail.com>
2023-08-20 16:26:10 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,15 @@ import Tabs from "./Tabs";
 const App = () => {
 
   const tabs=[
-    {
-      titles:"Tab 1" , contents : "Tab 1",
-    
-    },{
-      titles:"Tab 2" , contents : "Tab 2",
-      
-    },{
+      {
+      titles:"Tab 1" , contents : "Tab 1" 
+      },
+      {
+      titles:"Tab 2" , contents : "Tab 2"
+      },
+      {
       titles:"Tab 3" , contents : "Tab 3"
-    }
+      }
   ]
 
   return (
